2a9876b438e270ff69f18dfcd8d2e90a53ffa0dd
codin <douglasd3v.contato@gmail.com>
2025-05-08 11:16:56 -0300

    Rota para redirecionamento

--- src/routes/urlRoutes.mjs
+++ src/routes/urlRoutes.mjs
@@ -3,5 +3,6 @@ import UrlController from '../controllers/urlController.mjs';
 const router = express.Router();
 
 router.post('/url', UrlController.getUrlForShorten);
+router.get('/:shortCode', UrlController.redirectToOriginal);
 
 export default router;
