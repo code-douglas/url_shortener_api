7a3d729756bd08e03bf73681eda895fbf720c6c9
codin <douglasd3v.contato@gmail.com>
2025-05-08 11:53:31 -0300

    Alteração no controller para enviar data para o frontend

--- src/controllers/urlController.mjs
+++ src/controllers/urlController.mjs
@@ -3,47 +3,54 @@ import Url from '../models/Url.mjs';
 
 class UrlController {
   static async getUrlForShorten(req, res) {
-    const { originalUrl, expiresAt } = req.body;
+    const { originalUrl, expireAt } = req.body;
 
-    // Verificando se a URL original foi fornecida
     if (!originalUrl) {
       return res.status(400).json({ message: 'URL original é obrigatória.' });
     }
 
-    // Gerando o código curto com nanoid
     const shortCode = nanoid(8);
-    let expiresAtDate = undefined;
+    let expiresAt = undefined;
 
-    // Se a data de expiração for fornecida, processa ela
-    if (expiresAt) {
-      // Verifica se a data de expiração é válida
-      expiresAtDate = new Date(expiresAt);
-      if (isNaN(expiresAtDate)) {
+    if (expireAt) {
+      expiresAt = new Date(expireAt);
+      
+      if (isNaN(expiresAt)) {
         return res.status(400).json({ message: 'Data de expiração inválida.' });
       }
     }
-
+    
     try {
-      // Criação do objeto de URL no banco de dados
-      const url = new Url({
-        originalUrl,
-        shortCode,
-        expiresAt: expiresAtDate || null, // Se expiresAt não for fornecido, fica null
-      });
-
-      // Salvando no banco de dados
+      const url = new Url({ originalUrl, shortCode, expiresAt });
       await url.save();
 
-      // Retornando resposta com URL encurtada
       res.status(201).json({
         originalUrl,
         shortUrl: `${process.env.BASE_URL}/api/${shortCode}`,
-        expiresAt: expiresAtDate ? expiresAtDate.toISOString() : null, // Retornando a data no formato ISO
+        expiresAt: expiresAt ? expiresAt.toISOString() : null,
       });
     } catch (error) {
-      // Tratamento de erros
-      console.error(error);
       res.status(500).json({ message: 'Erro ao salvar a URL.' });
+    }
+  }
+
+  static async redirectToOriginal(req, res) {
+    const { shortCode } = req.params;
+
+    try {
+      const url = await Url.findOne({ shortCode });
+
+      if (!url) {
+        return res.status(404).json({ message: 'URL não encontrada.' });
+      }
+
+      if (url.expiresAt && url.expiresAt < new Date()) {
+        return res.status(410).json({ message: 'URL expirada.' });
+      }
+
+      res.redirect(url.originalUrl);
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao redirecionar.' });
     }
   }
 }
