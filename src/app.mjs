0ccc984f3bd039493347d6096aa9943839ac8aef
codin <douglasd3v.contato@gmail.com>
2025-05-08 12:17:33 -0300

    Alterando configuração do cors

--- src/app.mjs
+++ src/app.mjs
@@ -3,8 +3,11 @@ import cors from 'cors';
 import urlRoutes from './routes/urlRoutes.mjs';
 
 const app = express();
+const allowedOrigin = 'https://url-shortener-tau-roan.vercel.app/';
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigin
+}));
 app.use(express.json());
 
 app.use('/api', urlRoutes);
